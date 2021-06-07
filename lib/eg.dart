910e9c04a2ffcd11271292707307f7579e1bcf5c
Ayesha Tahir <ayeshatahirme@gmail.com>
2021-06-07 23:45:27 +0500

    ⚡ NEW: Added slider

--- lib/eg.dart
+++ lib/eg.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
+class CarouselDemo extends StatefulWidget {
+  CarouselDemo() : super();
+ 
+  final String title = "Carousel Demo";
+ 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CarouselDemoState(),
-    );
-  }
+  CarouselDemoState createState() => CarouselDemoState();
 }
-
-class CarouselDemoState extends StatefulWidget {
-
-  @override
-  _CarouselDemoStateState createState() => _CarouselDemoStateState();
-}
-
-class _CarouselDemoStateState extends State<CarouselDemoState> {
+ 
+class CarouselDemoState extends State<CarouselDemo> {
+  //
   CarouselSlider carouselSlider;
   int _current = 0;
   List imgList = [
-    'https://images.unsplash.com/photo-1623073798886-29e00ce6ab8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80',
-    'https://images.unsplash.com/photo-1623073798903-1f35bcb1f08a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=374&q=80',
-    'https://images.unsplash.com/photo-1623073799187-7f127a568266?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1574&q=80',
-    'https://images.unsplash.com/photo-1623073344463-67b7e23e9de9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=374&q=80',
-    'https://images.unsplash.com/photo-1554321586-92083ba0a115?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    'https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1554321586-92083ba0a115?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1536679545597-c2e5e1946495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1543922596-b3bbaba80649?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1502943693086-33b5b1cfdf2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
   ];
  
   List<T> map<T>(List list, Function handler) {
@@ -39,16 +29,12 @@ class _CarouselDemoStateState extends State<CarouselDemoState> {
     }
     return result;
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Video Player"),
-        centerTitle: true,
-        backgroundColor: Colors.purple[800],
-            elevation: 5,
-            shadowColor: Colors.grey[300],
+        title: Text(widget.title),
       ),
       body: Container(
         child: Column(
@@ -75,19 +61,10 @@ class _CarouselDemoStateState extends State<CarouselDemoState> {
                 return Builder(
                   builder: (BuildContext context) {
                     return Container(
-                      width: 300.0,
-//                      width: MediaQuery.of(context).size.width,
+                      width: MediaQuery.of(context).size.width,
                       margin: EdgeInsets.symmetric(horizontal: 10.0),
                       decoration: BoxDecoration(
                         color: Colors.green,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
                       ),
                       child: Image.network(
                         imgUrl,
@@ -105,12 +82,12 @@ class _CarouselDemoStateState extends State<CarouselDemoState> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: map<Widget>(imgList, (index, url) {
                 return Container(
-                  width: 7.0,
+                  width: 10.0,
                   height: 10.0,
                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: _current == index ? Colors.red[900] : Colors.grey,
+                    color: _current == index ? Colors.redAccent : Colors.green,
                   ),
                 );
               }),
@@ -136,13 +113,14 @@ class _CarouselDemoStateState extends State<CarouselDemoState> {
       ),
     );
   }
+ 
   goToPrevious() {
     carouselSlider.previousPage(
-        duration: Duration(milliseconds: 500), curve: Curves.ease);
+        duration: Duration(milliseconds: 300), curve: Curves.ease);
   }
  
   goToNext() {
     carouselSlider.nextPage(
-        duration: Duration(milliseconds: 500), curve: Curves.decelerate);
+        duration: Duration(milliseconds: 300), curve: Curves.decelerate);
   }
 }
